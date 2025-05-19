6cc022291b02b5e71e5abcf96aea7cef0d07375d
dietler <andy@dietler.net>
2025-05-19 13:53:23 -0700

    Enhance SchedulesList component with improved layout and additional details for better readability

--- app/(dashboard)/scheduled/component/list.tsx
+++ app/(dashboard)/scheduled/component/list.tsx
@@ -1,10 +1,18 @@
-"use client";
+'use client';
 
-import Loader from "@/app/components/loader";
-import { useGetSchedules } from "@/features/scheduled/api/use-get-schedules";
-import { Blockquote, Divider, Flex, Table, Tooltip } from "@mantine/core";
-import { IconAlertCircle, IconEdit, IconHistory } from "@tabler/icons-react";
-import { useRouter } from "next/navigation";
+import Loader from '@/app/components/loader';
+import { useGetSchedules } from '@/features/scheduled/api/use-get-schedules';
+import {
+  Blockquote,
+  Button,
+  Divider,
+  Flex,
+  SimpleGrid,
+  Table,
+  Tooltip,
+} from '@mantine/core';
+import { IconAlertCircle, IconEdit, IconHistory } from '@tabler/icons-react';
+import { useRouter } from 'next/navigation';
 
 const SchedulesList = () => {
   const router = useRouter();
@@ -12,43 +20,78 @@ const SchedulesList = () => {
 
   const rows = data?.map((element) => (
     <Table.Tr key={element.id}>
-      <Table.Td>{element.woNbr}</Table.Td>
-      <Table.Td>{element.woDescription}</Table.Td>
+      <Table.Td>
+        <SimpleGrid cols={1} spacing='xs' verticalSpacing='xs'>
+          <div className='text-sm font-bold'>{element.woNbr}</div>
+          <div className='text-md'>{element.woDescription}</div>
+        </SimpleGrid>
+      </Table.Td>
       <Table.Td>{element.woPrograma}</Table.Td>
       <Table.Td>{element.woRetirementGroup}</Table.Td>
-      <Table.Td>{element.scheduledStart}</Table.Td>
-      <Table.Td>{element.scheduledServicesStart}</Table.Td>
-      <Table.Td>{element.scheduledMainInstallComplete}</Table.Td>
-      <Table.Td>{element.scheduledAllServicesComplete}</Table.Td>
-      <Table.Td>{element.scheduledGasOnComplete}</Table.Td>
-      <Table.Td>{element.actualMainInstallFeet}</Table.Td>
-      <Table.Td>{element.targetMainInstallFeet}</Table.Td>
       <Table.Td>
-        <Flex gap={5}>
-          <Tooltip label="Edit" color="blue" withArrow arrowSize={6}>
-            <IconEdit
-              color={"blue"}
-              size={22}
-              style={{ cursor: "pointer" }}
-              onClick={() =>
-                router.push(`/scheduled/edit-schedule/${element.id}`)
-              }
-            />
-          </Tooltip>
-          <Divider size="sm" orientation="vertical" />
-          <Tooltip label="View Changes" color="teal" withArrow arrowSize={6}>
-            <IconHistory
-              color={"teal"}
-              size={22}
-              style={{ cursor: "pointer" }}
-              onClick={() =>
-                router.push(
-                  `/scheduled-changes/${element.id}?wo_nbr=${element.woNbr}`
-                )
-              }
-            />
-          </Tooltip>
-        </Flex>
+        <SimpleGrid cols={1} spacing='xs' verticalSpacing='xs'>
+          <div>
+            <div className='underline text-xs'>Scheduled Start</div>
+            <div>{element.scheduledStart}</div>
+          </div>
+          <div>
+            <div className='underline text-xs'>Scheduled Services Start</div>
+            <div>{element.scheduledServicesStart}</div>
+          </div>
+          <div>
+            <div className='underline text-xs'>
+              Scheduled Main Install Complete
+            </div>
+            <div>{element.scheduledMainInstallComplete}</div>
+          </div>
+          <div>
+            <div className='underline text-xs'>Scheduled Gas On Complete</div>
+            <div>{element.scheduledGasOnComplete}</div>
+          </div>
+          <div>
+            <div className='underline text-xs'>
+              Scheduled All Services Complete
+            </div>
+            <div>{element.scheduledAllServicesComplete}</div>
+          </div>
+        </SimpleGrid>
+      </Table.Td>
+      <Table.Td>
+        <SimpleGrid cols={1} spacing='xs' verticalSpacing='xs'>
+          <div>
+            <div className='underline text-xs'>Actual</div>
+            <div>{element.actualMainInstallFeet}</div>
+          </div>
+          <div>
+            <div className='underline text-xs'>Target</div>
+            <div>{element.targetMainInstallFeet}</div>
+          </div>
+        </SimpleGrid>
+      </Table.Td>
+      <Table.Td>
+        <SimpleGrid cols={1} spacing='xs' verticalSpacing='xs'>
+          <Button
+            leftSection={<IconEdit size={14} />}
+            variant='default'
+            onClick={() =>
+              router.push(`/scheduled/edit-schedule/${element.id}`)
+            }
+          >
+            Edit
+          </Button>
+
+          <Button
+            leftSection={<IconHistory size={14} />}
+            variant='default'
+            onClick={() =>
+              router.push(
+                `/scheduled-changes/${element.id}?wo_nbr=${element.woNbr}`
+              )
+            }
+          >
+            View Changes
+          </Button>
+        </SimpleGrid>
       </Table.Td>
     </Table.Tr>
   ));
@@ -60,20 +103,26 @@ const SchedulesList = () => {
   return (
     <>
       {rows?.length ? (
-        <Table.ScrollContainer minWidth={100} type="native">
-          <Table mt={16} striped highlightOnHover withTableBorder>
+        <Table.ScrollContainer minWidth={100} type='native'>
+          <Table
+            mt={16}
+            striped
+            highlightOnHover
+            withTableBorder
+            styles={{
+              td: { verticalAlign: 'top' },
+            }}
+          >
             <Table.Thead>
               <Table.Tr>
-                <Table.Th>WO Nbr.</Table.Th>
-                <Table.Th>WO Description</Table.Th>
-                <Table.Th>WO Programa</Table.Th>
-                <Table.Th>WO Retirement Group</Table.Th>
-                <Table.Th>Scheduled Start</Table.Th>
-                <Table.Th>Scheduled Main Install Complete</Table.Th>
-                <Table.Th>Scheduled Gas On Complete</Table.Th>
-                <Table.Th>Scheduled All Services Complete</Table.Th>
-                <Table.Th>Actual Main Install Feet</Table.Th>
-                <Table.Th>Target Main Install Feet</Table.Th>
+                <Table.Th>
+                  <div className='text-sm'>#</div>
+                  <div className='text-md'>Description</div>
+                </Table.Th>
+                <Table.Th>Program</Table.Th>
+                <Table.Th>Retirement Group</Table.Th>
+                <Table.Th>Dates</Table.Th>
+                <Table.Th>Main Install Feet</Table.Th>
                 <Table.Th></Table.Th>
               </Table.Tr>
             </Table.Thead>
@@ -81,8 +130,8 @@ const SchedulesList = () => {
           </Table>
         </Table.ScrollContainer>
       ) : (
-        <Blockquote color="pink" icon={<IconAlertCircle />} mt="xl">
-          <span className="text-slate-500">No results.</span>
+        <Blockquote color='pink' icon={<IconAlertCircle />} mt='xl'>
+          <span className='text-slate-500'>No results.</span>
         </Blockquote>
       )}
     </>
